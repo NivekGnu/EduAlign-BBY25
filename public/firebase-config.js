2702cbbb5e32dffc8260fd0c1bc833813c6a227b
Clinton Nguyen <nguyen.clinton@outlook.com>
2026-02-19 14:00:59 -0800

    Fixed reviewer page

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "?????????????????????????????",
+  apiKey: "AIzaSyB5NOkb1AcoAW3oKj7IzJWNIVvXoJSAPjk",
   authDomain: "edualignai-3800.firebaseapp.com",
   projectId: "edualignai-3800",
   storageBucket: "edualignai-3800.firebasestorage.app",
   messagingSenderId: "100283104152",
-  appId: "1:100283104152:web:fab2d9384f50593ac949ed"
+  appId: "1:100283104152:web:758224f9e5e89d08c949ed"
 };
 
 // Initialize Firebase
---
+++ utils/changeRole.js
@@ -0,0 +1,15 @@
+const { admin } = require('./firebase');
+require('dotenv').config();
+
+const email = process.argv[2];
+
+if (!email) {
+  console.log('Usage: node utils/changeRole.js <email>');
+  process.exit(1);
+}
+
+admin.auth().getUserByEmail(email)
+  .then(user => admin.auth().setCustomUserClaims(user.uid, { role: 'employee' }))
+  .then(() => console.log(`${email} promoted to employee`))
+  .catch(err => console.error('Error:', err.message))
+  .finally(() => process.exit());

255e3f0e366f001a272562f58ac59d5c9a23c6d9
Clinton Nguyen <nguyen.clinton@outlook.com>
2026-02-20 12:06:14 -0800

    Modified comment

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,5 +1,8 @@
+// Initializes Firebase Admin SDK for client-side
+// allows users to sign up, log in, log out, get authentication tokens
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB5NOkb1AcoAW3oKj7IzJWNIVvXoJSAPjk",
+  apiKey: "AIzaSyB5NOkb1AcoAW3oKj7IzJWNIVvXoJSAPjk", 
   authDomain: "edualignai-3800.firebaseapp.com",
   projectId: "edualignai-3800",
   storageBucket: "edualignai-3800.firebasestorage.app",
@@ -7,5 +10,4 @@ const firebaseConfig = {
   appId: "1:100283104152:web:758224f9e5e89d08c949ed"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
